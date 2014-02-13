90a10f50af14fadfc41cf0e6f1af4d248c5e3d87
Peter Suschlik <ps@neopoly.de>
2014-02-13 19:31:16 +0100

    WIP: Add features from RSpec

---
+++ features/step_definitions/around_steps.rb
@@ -0,0 +1,14 @@
+require 'tempfile'
+
+Given(/^a file named "(.*?)" with:$/) do |filename, content|
+  write_test_file(filename, content)
+end
+
+When(/^I run `ruby (.*?)`$/) do |filename|
+  path = path_for(filename)
+  @output = `ruby #{path}`
+end
+
+Then(/^the output should contain:$/) do |content|
+  assert_include @output, content
+end
